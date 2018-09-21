e42658a46fb891234ddb7a5067ea2fcf3918fe9f
sonota88 <yosiot8753@gmail.com>
2018-09-21 22:41:00 +0900

    Add sample data

--- data_table.js
+++ data_table.js
@@ -14,8 +14,25 @@ var data = [
         desc: "column description"
       }
     ]
+  },
+
+  {
+    name: "table2", // table name
+    pname: "{table name (physical)}",
+    desc: ["{line1}", "{line2}"],
+    cols: [
+      {
+        name: "{column name}",
+        pname: "{column name (physical)}",
+        pk: true, // primary key
+        required: true,
+        type: "int",
+        size: 11,
+        desc: ["{line1}", "{line2}"]
+      }
+    ]
   }
 
-  // ,{ ... } // table2
+  // ,{ ... } // table3
   // ...
 ];
