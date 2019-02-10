e38caa99f92b6cfa5ea27a5a0aee9de1b841136d
sonota88 <yosiot8753@gmail.com>
2019-02-10 11:13:05 +0900

    Add sample data (data_table.js)

--- data_table.js
+++ data_table.js
@@ -12,6 +12,15 @@ var data = [
         type: "int",
         size: 11,
         desc: "column description"
+      },
+      {
+        name: "{column name 2}",
+        pname: "{column name 2 (physical)}",
+        pk: false,
+        required: false,
+        type: "text",
+        size: 64,
+        desc: "http://example.com/foo/bar"
       }
     ]
   },
@@ -19,7 +28,7 @@ var data = [
   {
     name: "table2", // table name
     pname: "{table name (physical)}",
-    desc: ["{line1}", "{line2}"],
+    desc: ["{line1}", "{line2}", "http://example.com/foo/bar"],
     cols: [
       {
         name: "{column name}",
@@ -29,6 +38,15 @@ var data = [
         type: "int",
         size: 11,
         desc: ["{line1}", "{line2}"]
+      },
+      {
+        name: "{column name 2}",
+        pname: "{column name 2 (physical)}",
+        pk: true, // primary key
+        required: true,
+        type: "int",
+        size: 11,
+        desc: ["{line1}", "{line2}", "http://example.com/foo/bar"]
       }
     ]
   }
